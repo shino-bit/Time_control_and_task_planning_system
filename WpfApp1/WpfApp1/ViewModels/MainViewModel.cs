6a89b88bb9909fade74eefdbbc28f2a64ed042ab
Отовчиць Ілля Сергійович <ipz234_ois@student.ztu.edu.ua>
2026-03-02 00:27:51 +0200

    додано фільтрацію задач (Всі/Активні/Виконані) та візуальне оформлення списку з перекресленням завершених елементів

--- WpfApp1/WpfApp1/ViewModels/MainViewModel.cs
+++ WpfApp1/WpfApp1/ViewModels/MainViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Windows.Data;
 using System.Windows.Input;
 using System.Windows.Threading;
 using WpfApp1.Models;
@@ -16,20 +17,18 @@ namespace WpfApp1.ViewModels
         private readonly JsonDataService _dataService;
         private string _newTaskTitle = string.Empty;
         private TaskItem _selectedTask;
-
+        
         private DispatcherTimer _timer;
         private bool _isTimerRunning;
+        private string _currentFilter = "All";
 
         public ObservableCollection<TaskItem> Tasks { get; set; }
+        public ICollectionView TasksView { get; } 
 
         public string NewTaskTitle
         {
             get => _newTaskTitle;
-            set
-            {
-                _newTaskTitle = value;
-                OnPropertyChanged();
-            }
+            set { _newTaskTitle = value; OnPropertyChanged(); }
         }
 
         public TaskItem SelectedTask
@@ -37,11 +36,7 @@ namespace WpfApp1.ViewModels
             get => _selectedTask;
             set
             {
-                if (_isTimerRunning)
-                {
-                    StopTimer(null);
-                }
-
+                if (_isTimerRunning) StopTimer(null);
                 _selectedTask = value;
                 OnPropertyChanged();
             }
@@ -50,114 +45,99 @@ namespace WpfApp1.ViewModels
         public bool IsTimerRunning
         {
             get => _isTimerRunning;
-            set
-            {
-                _isTimerRunning = value;
-                OnPropertyChanged();
-            }
+            set { _isTimerRunning = value; OnPropertyChanged(); }
         }
 
         public ICommand AddTaskCommand { get; }
         public ICommand DeleteTaskCommand { get; }
-
         public ICommand StartTimerCommand { get; }
         public ICommand StopTimerCommand { get; }
+        public ICommand FilterCommand { get; }
 
         public MainViewModel()
         {
             _dataService = new JsonDataService();
-
             var loadedTasks = _dataService.LoadTasks();
             Tasks = new ObservableCollection<TaskItem>(loadedTasks);
 
+            TasksView = CollectionViewSource.GetDefaultView(Tasks);
+            TasksView.Filter = FilterTasksCondition;
+
             Tasks.CollectionChanged += Tasks_CollectionChanged;
+            foreach (var task in Tasks) task.PropertyChanged += Task_PropertyChanged;
 
-            foreach (var task in Tasks)
-            {
-                task.PropertyChanged += Task_PropertyChanged;
-            }
-
-            _timer = new DispatcherTimer();
-            _timer.Interval = TimeSpan.FromSeconds(1);
+            _timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(1) };
             _timer.Tick += Timer_Tick;
 
             AddTaskCommand = new RelayCommand(AddTask, CanAddTask);
             DeleteTaskCommand = new RelayCommand(DeleteTask, CanDeleteTask);
-
             StartTimerCommand = new RelayCommand(StartTimer, CanStartTimer);
             StopTimerCommand = new RelayCommand(StopTimer, CanStopTimer);
+            FilterCommand = new RelayCommand(ChangeFilter);
+        }
+
+        private bool FilterTasksCondition(object obj)
+        {
+            if (obj is TaskItem task)
+            {
+                return _currentFilter switch
+                {
+                    "Active" => !task.IsCompleted,
+                    "Completed" => task.IsCompleted,
+                    _ => true
+                };
+            }
+            return false;
+        }
+
+        private void ChangeFilter(object parameter)
+        {
+            if (parameter is string filter)
+            {
+                _currentFilter = filter;
+                TasksView.Refresh(); 
+            }
         }
 
         private void Timer_Tick(object sender, EventArgs e)
         {
-            if (SelectedTask != null)
-            {
-                SelectedTask.TimeSpentSeconds++;
-            }
+            if (SelectedTask != null) SelectedTask.TimeSpentSeconds++;
         }
 
         private bool CanStartTimer(object parameter) => SelectedTask != null && !IsTimerRunning;
-        private void StartTimer(object parameter)
-        {
-            IsTimerRunning = true;
-            _timer.Start();
-        }
+        private void StartTimer(object parameter) { IsTimerRunning = true; _timer.Start(); }
 
         private bool CanStopTimer(object parameter) => IsTimerRunning;
-        private void StopTimer(object parameter)
-        {
-            IsTimerRunning = false;
-            _timer.Stop();
-            SaveData(); 
-        }
+        private void StopTimer(object parameter) { IsTimerRunning = false; _timer.Stop(); SaveData(); }
 
         private void Tasks_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
         {
-            if (e.NewItems != null)
-            {
-                foreach (TaskItem item in e.NewItems) item.PropertyChanged += Task_PropertyChanged;
-            }
-            if (e.OldItems != null)
-            {
-                foreach (TaskItem item in e.OldItems) item.PropertyChanged -= Task_PropertyChanged;
-            }
+            if (e.NewItems != null) foreach (TaskItem item in e.NewItems) item.PropertyChanged += Task_PropertyChanged;
+            if (e.OldItems != null) foreach (TaskItem item in e.OldItems) item.PropertyChanged -= Task_PropertyChanged;
         }
 
         private void Task_PropertyChanged(object sender, PropertyChangedEventArgs e)
         {
-            if (e.PropertyName == nameof(TaskItem.TimeSpentSeconds) && IsTimerRunning)
-            {
-                return;
-            }
+            if (e.PropertyName == nameof(TaskItem.TimeSpentSeconds) && IsTimerRunning) return;
+
+            if (e.PropertyName == nameof(TaskItem.IsCompleted)) TasksView.Refresh();
+            
             SaveData();
         }
 
         private bool CanAddTask(object parameter) => !string.IsNullOrWhiteSpace(NewTaskTitle);
         private void AddTask(object parameter)
         {
-            var newTask = new TaskItem { Title = NewTaskTitle };
-            Tasks.Add(newTask);
-            NewTaskTitle = string.Empty;
+            Tasks.Add(new TaskItem { Title = NewTaskTitle });
+            NewTaskTitle = string.Empty; 
         }
 
         private bool CanDeleteTask(object parameter) => SelectedTask != null;
-        private void DeleteTask(object parameter)
-        {
-            if (SelectedTask != null)
-            {
-                Tasks.Remove(SelectedTask);
-            }
-        }
+        private void DeleteTask(object parameter) { if (SelectedTask != null) Tasks.Remove(SelectedTask); }
 
-        private void SaveData()
-        {
-            _dataService.SaveTasks(Tasks.ToList());
-        }
+        private void SaveData() => _dataService.SaveTasks(Tasks.ToList());
 
         public event PropertyChangedEventHandler PropertyChanged;
-        protected void OnPropertyChanged([CallerMemberName] string name = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-        }
+        protected void OnPropertyChanged([CallerMemberName] string name = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
     }
 }
